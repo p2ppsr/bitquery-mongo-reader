a242d54465019106a262c69f27615c9503b515bc
guru <guru@unb.home>
2022-08-01 23:11:02 +0200

    updated build-docker.sh with ump-reader to bitquery-mongo-reader

--- build-docker.sh
+++ build-docker.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-docker build -t us.gcr.io/computing-with-integrity/ump-reader .
-docker push us.gcr.io/computing-with-integrity/ump-reader
+docker build -t us.gcr.io/computing-with-integrity/bitquery-mongo-reader .
+docker push us.gcr.io/computing-with-integrity/bitquery-mongo-reader
